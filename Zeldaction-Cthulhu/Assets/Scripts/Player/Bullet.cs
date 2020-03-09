f843a293a6d3d1e20f4fcd40b03360069b135ad0
Arthur SPELLANI <a.spellani@rubika-edu.com>
2020-03-09 14:10:34 +0100

    Player Shoot (Direction + Slow + CD)

---
+++ Zeldaction-Cthulhu/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Enemy;
+
+namespace Player
+{
+    public class Bullet : MonoBehaviour
+    {
+
+        public Rigidbody2D rb;
+
+        
+
+    	void Awake()
+	    {
+	    
+	    }
+    
+        void Start()
+        {
+            rb.velocity = PlayerManager.Instance.playerShoot.shootDirection * PlayerManager.Instance.playerShoot.bulletSpeed;
+        }
+    
+        void Update()
+        {
+            
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                //collision.GetComponent<EnemyBasicBehaviour>.TakeDamage();
+            }
+
+            if (collision.gameObject.tag == "Enviro")
+            {
+                Destroy(this);
+            }
+
+        }
+
+    }
+}
---
+++ Zeldaction-Cthulhu/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerShoot : MonoBehaviour
+    {
+
+        [HideInInspector] public Vector2 shootDirection;
+        bool directionStored = false;
+        bool canShoot;
+
+        [Range(0,50)]
+        public int aimSlow;
+
+        [Range(0, 20)]
+        public int bulletSpeed;
+
+        public GameObject bullet;
+        public float shootCooldown;
+
+        [HideInInspector] public bool isShooting = false;
+
+
+        void Awake()
+	    {
+	    
+	    }
+    
+        void Start()
+        {
+            canShoot = true;
+        }
+    
+        void Update()
+        {
+
+            if (Input.GetAxisRaw("Shoot") != 0)
+            {
+               AimMovement();                
+            }
+
+            if (Input.GetAxisRaw("Shoot") == 0 && isShooting == true)
+            {
+               ExitShoot();
+            }
+        }
+
+        void AimMovement()
+        {
+            if(directionStored == false)
+            {
+                GetDirection();
+            }
+
+            if (Input.GetButtonDown("Attack") && canShoot == true)
+            {
+                ShootBullet();
+            }
+
+        }
+    
+        void GetDirection()
+        {
+            shootDirection = PlayerManager.Instance.playerMovement.currentDirection;
+            PlayerManager.Instance.playerMovement.speed -= aimSlow;
+            directionStored = true;
+            isShooting = true;
+        }
+
+        void ExitShoot()
+        {
+            directionStored = false;
+            PlayerManager.Instance.playerMovement.speed += aimSlow;
+            isShooting = false;
+        }
+
+        void ShootBullet()
+        {            
+            StartCoroutine(ShootDelay());
+            Instantiate(bullet, this.transform);
+        }
+
+        IEnumerator ShootDelay()
+        {
+            canShoot = false;
+            yield return new WaitForSeconds(shootCooldown);
+            canShoot = true;
+        }
+
+    }
+}
