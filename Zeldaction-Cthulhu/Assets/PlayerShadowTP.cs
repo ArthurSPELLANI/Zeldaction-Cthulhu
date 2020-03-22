5b7378279bb503252aedc0c407e2859f5ac85d1c
Arthur SPELLANI <a.spellani@rubika-edu.com>
2020-03-22 15:13:10 +0100

    Fix Shadow

---
+++ Zeldaction-Cthulhu/Assets/PlayerShadowTP.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shadow
+{
+    public class PlayerShadowTP : MonoBehaviour
+    {
+
+        void Awake()
+        {
+            
+        }
+
+        void Start()
+        {
+
+        }
+        
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log("sa touche");
+        }
+    }
+}
+
+
