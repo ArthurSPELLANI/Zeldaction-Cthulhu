cd3a0dc2b682b1d35391e27df4c74d6500735e91
Arthur SPELLANI <a.spellani@rubika-edu.com>
2020-04-13 14:50:00 +0200

    Pickups

---
+++ Zeldaction-Cthulhu/Assets/PickupLife.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+public class PickupLife : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Shadow")
+        {
+            if (PlayerManager.Instance.playerStats.playerCurrentHealth == PlayerManager.Instance.playerStats.playerMaxHealth -1)
+            {
+                PlayerManager.Instance.playerStats.playerCurrentHealth += 1;
+                Destroy(gameObject);
+            }
+
+            if (PlayerManager.Instance.playerStats.playerCurrentHealth < PlayerManager.Instance.playerStats.playerMaxHealth)
+            {
+                PlayerManager.Instance.playerStats.playerCurrentHealth += 2;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
