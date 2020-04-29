b9c803e7303ac92c7fc92f2d04f91523da705dcf
Arthur SPELLANI <a.spellani@rubika-edu.com>
2020-04-29 12:20:07 +0200

    Patch Sprint 9

---
+++ Zeldaction-Cthulhu/Assets/Scripts/Dialog&PNJ/ColliderCutScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+
+public class ColliderCutScene : MonoBehaviour
+{
+   // public SecondCinematic SecondCinematic;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+         //   SecondCinematic.SetActive(true);
+        }
+    }
+}
