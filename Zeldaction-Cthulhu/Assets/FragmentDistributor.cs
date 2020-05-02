61abc44c5ab000ba333de9d5bbe642beb8c61168
Arthur SPELLANI <a.spellani@rubika-edu.com>
2020-05-02 17:58:57 +0200

    Fragment Distributor

---
+++ Zeldaction-Cthulhu/Assets/FragmentDistributor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FragmentDistributor : MonoBehaviour
+{
+    public bool isCombat;
+    public bool isPuzzle;
+
+    public GameObject pillarToActivate;
+    public List<GameObject> enemiesToKill;
+
+    public GameObject fragment;
+    Quaternion rotation = Quaternion.identity;
+
+
+    private void Update()
+    {
+        if (isCombat == true)
+        {
+            if(enemiesToKill[0] == null)
+            {
+                Instantiate(fragment, transform.position, rotation);
+                Destroy(gameObject);
+            }
+        }
+
+
+        if (isPuzzle == true)
+        {
+            if(pillarToActivate.gameObject.activeSelf == true)
+            {
+                Instantiate(fragment, transform.position, rotation);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+}
