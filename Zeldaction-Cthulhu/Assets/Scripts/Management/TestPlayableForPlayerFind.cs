33e725cb7be199723b39709dbaa15ec470a0ba39
Arthur SPELLANI <a.spellani@rubika-edu.com>
2020-05-05 12:05:41 +0200

    TestPlayableForPlayer: Le truc qui permet de get le joueur dans timeline

---
+++ Zeldaction-Cthulhu/Assets/Scripts/Management/TestPlayableForPlayerFind.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using Player;
+
+public class TestPlayableForPlayerFind : MonoBehaviour
+{
+
+    public PlayableDirector playableDirector;
+    public PlayableAsset playableAsset;
+
+    GameObject player;
+
+    public Animator[] trackAnimators = new Animator[2];
+    public GameObject[] trackObjects = new GameObject[7];
+
+    public void Start()
+    {
+        player = GameObject.Find("Player");
+
+        trackAnimators[0] = player.GetComponent<Animator>();
+        trackAnimators[1] = player.GetComponentInChildren<Animator>();
+        trackObjects[0] = GameObject.Find("Attack");
+        trackObjects[1] = GameObject.Find("Movement");
+        trackObjects[2] = GameObject.Find("Attack");
+        trackObjects[3] = GameObject.Find("ShadowMode");
+        trackObjects[4] = GameObject.Find("Coeurs");
+        trackObjects[5] = GameObject.Find("Ammunitions");
+        trackObjects[6] = GameObject.Find("Sanity Gauge");
+        
+        var outputs = playableAsset.outputs;
+        foreach (var itm in outputs)
+        {
+            //Debug.Log(itm.streamName);
+            if (itm.streamName == "Player")
+            {
+                playableDirector.SetGenericBinding(itm.sourceObject, trackAnimators[0]);
+            }
+            if (itm.streamName == "Graphics")
+            {
+                playableDirector.SetGenericBinding(itm.sourceObject, trackAnimators[1]);
+            }
+            if (itm.streamName == "Attack")
+            {
+                playableDirector.SetGenericBinding(itm.sourceObject, trackObjects[0]);
+            }
+            if (itm.streamName == "Movement")
+            {
+                playableDirector.SetGenericBinding(itm.sourceObject, trackObjects[1]);
+            }
+            if (itm.streamName == "Shoot")
+            {
+                playableDirector.SetGenericBinding(itm.sourceObject, trackObjects[2]);
+            }
+            if (itm.streamName == "ShadowMode")
+            {
+                playableDirector.SetGenericBinding(itm.sourceObject, trackObjects[3]);
+            }
+            if (itm.streamName == "Coeurs")
+            {
+                playableDirector.SetGenericBinding(itm.sourceObject, trackObjects[4]);
+            }
+            if (itm.streamName == "Ammunitions")
+            {
+                playableDirector.SetGenericBinding(itm.sourceObject, trackObjects[5]);
+            }
+            if (itm.streamName == "SanityGauge")
+            {
+                playableDirector.SetGenericBinding(itm.sourceObject, trackObjects[6]);
+            }
+
+
+
+
+        }
+    }
+}
