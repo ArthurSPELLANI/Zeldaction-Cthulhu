148cf9106db648611d9913ace3583c2719fdd0fe
Lolzxad <adelou001@hotmail.com>
2020-05-06 17:47:33 +0200

    Pitfall new respawn DONE and TESTED

---
+++ Zeldaction-Cthulhu/Assets/Scripts/LevelDesignElement/RespawnLocking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelDesign
+{
+    public class RespawnLocking : MonoBehaviour
+    {
+        public bool respawnUnlocked = false;
+
+        void OnTriggerEnter2D (Collider2D player)
+        {
+            if (player.gameObject.tag == "Player")
+            {
+                respawnUnlocked = true;
+            }
+        }
+    }
+}
