ed13cc7833c7a73043110e72b527f9b09b04870d
PARET Paul <paret.paul@orange.fr>
2020-05-07 00:50:58 +0200

    Text Mesh Pro basic Added & Main Menu Done

---
+++ Zeldaction-Cthulhu/Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void ResumeGame()
+        {
+            //SceneManager.LaodScene(LaSceneSauvegardé)
+        }
+
+        public void NewGame()
+        {
+            //Emmène sur la scene qui se trouve après la scene en cours
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+            //Reset la sauvegarde du joueur
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("You Quit");
+            Application.Quit();
+        }
+    }
+}
+
