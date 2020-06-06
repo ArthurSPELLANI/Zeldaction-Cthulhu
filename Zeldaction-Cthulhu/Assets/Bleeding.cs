27e1fc28fe9485370d4152c6e000a47ee717bd05
Quentin <q.kerhello@rubika-edu.com>
2020-06-06 18:38:03 +0200

    Bleeding states graphs + integration

---
+++ Zeldaction-Cthulhu/Assets/Bleeding.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Player;
+
+public class Bleeding : MonoBehaviour
+{
+    public Image[] bleeding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bleeding[0].color = new Color(1, 1, 1, 0);
+        bleeding[1].color = new Color(1, 1, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerManager.Instance.playerStats.playerCurrentHealth == 2)
+        {
+            bleeding[0].color = new Color(1, 1, 1, 1);
+            bleeding[1].color = new Color(1, 1, 1, 0);
+        }
+        else if(PlayerManager.Instance.playerStats.playerCurrentHealth == 1)
+        {
+            bleeding[0].color = new Color(1, 1, 1, 0);
+            bleeding[1].color = new Color(1, 1, 1, 1);
+        }
+        else
+        {
+            bleeding[0].color = new Color(1, 1, 1, 0);
+            bleeding[1].color = new Color(1, 1, 1, 0);
+        }
+    }
+}
