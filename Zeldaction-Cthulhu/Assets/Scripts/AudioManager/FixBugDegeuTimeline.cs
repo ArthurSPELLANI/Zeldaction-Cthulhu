8bf400806d3269bf886b73a800e128c199c65c22
Alex <a.lepine@rubika-edu.com>
2020-06-09 10:01:23 +0200

    Les sons de dialogues de la grotte

---
+++ Zeldaction-Cthulhu/Assets/Scripts/AudioManager/FixBugDegeuTimeline.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioManaging;
+
+public class FixBugDegeuTimeline : MonoBehaviour
+{
+
+    TimelineManager TM;
+    void Start()
+    {
+        TM = gameObject.GetComponent<TimelineManager>();
+
+        StartCoroutine(Reglagedesons());
+    
+
+    }
+
+    IEnumerator Reglagedesons()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+
+        foreach (Sound s in TM.Son)
+        {
+            s.source.volume = 0.000001f;
+        }
+    }
+
+    public void ReReglageSon()
+    {
+        foreach (Sound s in TM.Son)
+        {
+            s.source.volume = 0.5f * AudioManager.Instance.volumeSounds;
+        }
+    }
+}
