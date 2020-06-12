ca1ea8fb39a3a3a16355de7f322c6a5a505eb99c
Quentin <q.kerhello@rubika-edu.com>
2020-06-12 16:02:05 +0200

    End Cinematic

---
+++ Zeldaction-Cthulhu/Assets/End_Cinematic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class End_Cinematic : MonoBehaviour
+{
+    public GameObject video;
+    public GameObject canva;
+    public Image fade;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnEnable()
+    {
+        StartCoroutine(FadeTo(1, 3));
+        
+    }
+
+    public void StartVideo()
+    {
+        
+        canva.SetActive(false);
+        video.SetActive(true);
+        
+    }
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        Debug.Log("debut fade");
+        float alpha = fade.color.a;
+        for (float t = 0.0f; t < 3.0f; t += Time.deltaTime / aTime)
+        {
+            Color newColor = new Color(0, 0, 0, Mathf.Lerp(alpha, aValue, t));
+            fade.color = newColor;
+            Debug.Log("fade");
+
+            yield return null;
+        }
+        StartVideo();
+        yield return new WaitForSeconds(0.2f);
+        fade.gameObject.SetActive(false);
+    }
+}
